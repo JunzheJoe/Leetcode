d40114786fc122c8f658961afb50fe843fee5150
rx-oo <gimezjz10@gmail.com>
2025-01-29 22:51:04 -0500

    second commit

--- Leetcode/Array & Hashing/1. Two Sum/sol.py
+++ Leetcode/Array & Hashing/1. Two Sum/sol.py
@@ -1,14 +1,10 @@
-
-# Python implementation of Insertion Sort
-def insertionSort(arr):
-	# Traverse through 1 to len(arr)
-    for i in range(1, len(arr)):
-        j = i - 1
-        while j >= 0 and arr[j + 1] < arr[j]:
-            # arr[j] and arr[j + 1] are out of order so swap them 
-            tmp = arr[j + 1]
-            arr[j + 1] = arr[j]
-            arr[j] = tmp
-            j -= 1
-    return arr
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hash_map:
+                return [hash_map[diff], i]
+            hash_map[n] = i
+        return
 
